e70e117d8bba8ff93adfff2d30016d9d0c6dbfd5
alejandro <squella100@gmail.com>
2018-06-02 12:30:46 +0200

    mi tercer commit

--- fibonacci_TDD.js
+++ fibonacci_TDD.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
 
-function fibonacci(){
-    return 1;
+function fibonacci(n){
+    if(n===0){
+        return 1;
+    }
+    return n;
 }
 
 console.log('Fibbonaci TDD en mi Github');
 
 assert.equal(fibonacci(0),1);
+assert.equal(fibonacci(1),1);
+assert.equal(fibonacci(2),2);
